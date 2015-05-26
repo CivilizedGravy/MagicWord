54191931296c10c7b1773ac5a50c2a48a1e88c33
Ryan <Civilizedgravy@ip-10-138-111-216.ec2.internal>
2015-05-26 17:16:52 +0000

    cleaned up

--- magic_number_project/magic_number_project/settings.py
+++ magic_number_project/magic_number_project/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'magic_number_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':  'Civilizedgravy$magic_word',
+        'HOST': 'mysql.server',
+        'USER': 'Civilizedgravy',
+        'PASSWORD': 'Bootyspaghetti',
+
     }
 }
 
--- magic_number_project/magic_number_project/wsgi.py
+++ magic_number_project/magic_number_project/wsgi.py
@@ -6,11 +6,23 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
+activate_this = '/home/Civilizedgravy/magic_word_env/bin/activate_this.py'
+execfile(activate_this, dict(__file__=activate_this))
 
 import os
+import sys
+
+path = '/home/Civilizedgravy/magic_word_project'
+
+if path not in sys.path:
+    sys.path.append(path)
+
+os.chdir(path)
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "magic_number_project.settings")
+import django
+django.setup()
 
 application = get_wsgi_application()
